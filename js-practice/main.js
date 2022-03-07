7fce77fa395407b6ecbaa56c97ac72f6915b670a
Akuby <akuby1997@gmail.com>
2022-03-07 23:32:43 +0900

    extends keyword practice

--- js-practice/main.js
+++ js-practice/main.js
@@ -1,49 +1,37 @@
-// 함수 복습
+// function user(first, last){
+//   this.firstName = first;
+//   this.lastName = last;
+// }
 
-function sum(x, y) { //x와 y는 매개변수 (Parameter)
-  console.log(x + y);
-}
+// user.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`
+// }
 
-sum(1, 3); //1과 3은 인수 (Argument)
-sum(2, 7);
-
-function sum1(x, y) {
-  return x + y
-}
-
-console.log(sum1(1, 3));
-console.log(sum1(3, 2));
-console.log(sum1(1, 3) + sum1(3, 2)); //이러면 함수를 두번 돌린다.
-
-const a = sum1(1, 3),
-  b = sum1(3, 2); //이렇게 다른 변수에 할당해놓으면 함수 호출을 줄일 수 있다.
-//적은 수라도 뭔가가 반복되는 경우에는 따로 할당하는게 좋을듯.
-console.log(a);
-console.log(b);
-console.log(a + b);
-
-
-const 함수 = function (a, b) { //익명 함수. 함수를 변수에 담아 한번만 쓴다.
-  return a * b;
-}
-
-function 함수1(a, b) { //기명 함수. 함수 명을 통해 여러번 쓴다.
-  return a * b;
-}
-
-//return 키워드
-
-function sum2(x, y) {
-  if (x < 2) {
-    return //return 키워드는 함수를 "종료"한다.
+class Vehicle {
+  constructor(name, wheel) {
+    this.name = name;
+    this.wheel = wheel;
   }
-  return x + y
 }
 
-// argument 객체
+const myVehicle = new Vehicle('운송수단', 2)
+console.log(myVehicle);
 
-function argu() { //argument는 받은 인자를 배열로 반환한다.
-  return arguments[0] + arguments[1];
+class Bicycle extends Vehicle {
+  // constructor(name, wheel){
+  //   super(name, wheel)
+  // }
 }
 
-console.log(argu(3, 7))
+const myBicycle = new Bicycle('삼천리', 2);
+console.log(myBicycle);
+
+class Car extends Vehicle {
+  constructor(name, wheel, license){
+    super(name, wheel)
+    this.license = license
+  }
+}
+
+const myCar = new Car('벤츠', 4, true)
+console.log(myCar)
