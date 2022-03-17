9d745dc0ec852cc56609a9286b1caac0005a0dd5
Akuby <akuby1997@gmail.com>
2022-03-17 19:40:03 +0900

    vue3 practicing

---
+++ vue3-test/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1
+  :class="{ active : isActive }" 
+  @click="activate">
+  Hello? {{ isActive }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      isActive : false
+    }
+  },
+  methods : {
+    activate(){
+      this.isActive = true
+    }
+  }
+}
+</script>
+
+<style>
+  .active {
+    font-size : 40px;
+    color : red;
+  }
+</style>
