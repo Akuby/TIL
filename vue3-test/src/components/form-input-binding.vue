009a584df6e3261dd49996f1b505d0b7bc1364b8
Akuby <akuby1997@gmail.com>
2022-03-19 03:09:07 +0900

    form-input-binding

---
+++ vue3-test/src/components/form-input-binding.vue
@@ -0,0 +1,19 @@
+<template>
+  <input 
+  type="text" 
+  @input="msg = $event.target.value">
+  {{ msg }}
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        "msg" : this.msg
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>>
