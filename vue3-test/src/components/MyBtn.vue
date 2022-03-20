8cf7ebd2cfc3350dcda710385b06294bf33033db
Akuby <akuby1997@gmail.com>
2022-03-20 17:55:05 +0900

    dealing with components

---
+++ vue3-test/src/components/MyBtn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+  :style="{ 'background-color' : color}"
+  :class="{ large }"
+  class="btn">
+  <slot></slot></div>
+</template>
+
+<script>
+export default {
+  props:{
+    color: {
+      type:String,
+      default:'gray'
+    },
+    large : {
+      type: Boolean,
+      default : false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .btn{
+    margin: 4px;
+    padding: 12px 24px;
+    border-radius: 4px;
+    display: inline-block;
+    cursor: pointer;
+    &.large{
+      font-size: 30px;
+      padding : 15px 27px
+    }
+  }
+</style>
